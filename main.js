488f84cdca8caf9b55d46c8e07ad3c1ee40c2742
Shaohua Zhou <shao@versal.com>
2014-08-14 16:02:13 -0700

    First step using versal-player-api

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 (function(){
+  /* global VersalPlayerAPI */
+
   // Declare a gadget class.
   var Gadget = function(options) {
+    // versal interface
+    this.vi = new VersalPlayerAPI();
+
     // the main DOM element for attaching
     this.el = options.el;
     // the selected word
@@ -21,45 +26,26 @@
     this.initialize();
   };
 
-  // Simple helper functions: forwarding JSON messages and logging to console.
-  Gadget.prototype.receiveMessage = function (messageObj) {
-    var messageJson = messageObj.data;
-    // a received message has the structure { .... data: { event: 'eventName', data: { ... } } ... }
-    // received messages are of MessageEvent class.
-
-    console.log('Received message', messageObj);
-
-    // We will call the gadget's method named by the event, if this method exists.
-    if (this[messageJson.event]) {
-      this[messageJson.event](messageJson.data);
-    }
-  };
-
-  Gadget.prototype.sendMessage = function (messageJson) {
-    console.log('Sending message', messageJson);
-    // an outgoing message has the structure {event: 'eventName', data: ... }
-    window.parent.postMessage(messageJson, '*');
-  };
-
   // Need to configure the property sheet after attaching.
   Gadget.prototype.setupPropertySheet = function() {
     // set up a property sheet for word and color selection.
-    this.sendMessage({
-      event: 'setPropertySheetAttributes',
-      data: {
-        chosenColor: { type: 'Color' },
-        chosenWord: { type: 'Text' }
-      }
+    this.vi.setPropertySheetAttributes({
+      chosenColor: { type: 'Color' },
+      chosenWord: { type: 'Text' }
     });
   };
 
   // Initialize: before the gadget is attached to the lesson's DOM.
   Gadget.prototype.initialize = function() {
-      // subscribe to player events.
-      this.sendMessage({event: 'startListening'});
-      window.addEventListener('message', this.receiveMessage.bind(this));
+      //
+      this.vi.on('attributesChanged', this.attributesChanged.bind(this));
+      this.vi.on('learnerStateChanged', this.learnerStateChanged.bind(this));
+
 
       this.setupPropertySheet();
+
+      // subscribe to player events.
+      this.vi.startListening();
 
       // add click listener to toggle bold font.
       this.wordEl.onclick = this.toggleBoldWord.bind(this);
@@ -68,15 +54,10 @@
       this.el.querySelector('.button-upload-image').onclick = this.requestUpload.bind(this);
 
       // set gadget height.
-      this.sendMessage({
-        event: 'setHeight',
-        data: {
-          pixels: 400
-        }
-      });
+      this.vi.setHeight(400);
 
       // initially the gadget is already not empty (it has "green" set). If it were otherwise, we would have done this:
-      // this.setEmpty();
+      // this.vi.setEmpty();
 
   };
 
@@ -104,12 +85,6 @@
       var item = elementsAuthoringOnly[i];
       item.setAttribute('style', 'visibility: ' + visibilityForAuthor + ';');
     }
-  };
-
-  Gadget.prototype.setEmpty = function () {
-    this.sendMessage({
-      event: 'setEmpty'
-    });
   };
 
   // this will request an image URL.
@@ -169,11 +144,8 @@
 
   Gadget.prototype.toggleBoldWord = function() {
     this.config.learnerState.isBold = ! this.config.learnerState.isBold;
-    this.sendMessage({
-      event: 'setLearnerState',
-      data: {
-        isBold: this.config.learnerState.isBold
-      }
+    this.vi.setLearnerState({
+      isBold: this.config.learnerState.isBold
     });
     this.updateBoldWord();
   };
